528c8eb7b7ceacb424c16200ff55006e821197ff
CY-Cheung <c.y.cheung.contact@gmail.com>
2025-08-05 17:56:54 +0800

    Quick commit by script.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,11 +2,9 @@ import Button from '../../Components/Button/Button'
 import './Home.css'
 
 function Home() {
-  const angles = Array.from({ length: 36 }, (_, i) => i * 10); // 0~350, step 10
-
   return (
     <div className="home">
-      <Button text="Click Me" fontSize="3rem" angle={200} />
+      <Button text="Button" fontSize="3rem" angle={0} />
     </div>
   );
 }
