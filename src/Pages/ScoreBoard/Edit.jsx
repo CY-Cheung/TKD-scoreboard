c0d7ce4ee095070a6280ccf51cf0b90a27e97108
CY-Cheung <c.y.cheung.contact@gmail.com>
2025-08-09 01:51:47 +0800

    Create edit score button.

--- src/Pages/ScoreBoard/Edit.jsx
+++ src/Pages/ScoreBoard/Edit.jsx
@@ -5,25 +5,45 @@ function Edit({ visible, setVisible }) {
     if (!visible) return null; // 不顯示時直接不渲染
     return (
         <div className="edit">
-            Edit <Button text="Done" onClick={() => setVisible(false)} />
-            <div>
-                Blue
-                <Button text="Gam-jeom" angle={190} />
-                <Button text="Punch" angle={190} />
-                <Button text="Body" angle={190} />
-                <Button text="Head" angle={190} />
-                <Button text="Spin Body" angle={190} />
-                <Button text="Spin Head" angle={190} />
+            <h2>Edit</h2>
+            <div className="edit-score">
+                <div className="column">
+                    <div>Score</div>
+                    <div>Blue</div>
+                    <div>Red</div>
+                </div>
+                <div className="column">
+                    <div>Gam-jeom</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
+                <div className="column">
+                    <div>Punch (1)</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
+                <div className="column">
+                    <div>Body (2)</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
+                <div className="column">
+                    <div>Head (3)</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
+                <div className="column">
+                    <div>Tech Body (4)</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
+                <div className="column">
+                    <div>Tech Head (5)</div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={210} fontSize={"1.25vw"}/><Button text={"-1"} angle={210} fontSize={"1.25vw"}/></div>
+                    <div className="edit-score-button"><Button text={"+1"} angle={350} fontSize={"1.25vw"}/><Button text={"-1"} angle={350} fontSize={"1.25vw"}/></div>
+                </div>
             </div>
-            <div>
-                Red
-                <Button text="Gam-jeom" angle={350} />
-                <Button text="Punch" angle={350} />
-                <Button text="Body" angle={350} />
-                <Button text="Head" angle={350} />
-                <Button text="Spin Body" angle={350} />
-                <Button text="Spin Head" angle={350} />
-            </div>
+            <Button text="Done" onClick={() => setVisible(false)} />
         </div>
     );
 }
