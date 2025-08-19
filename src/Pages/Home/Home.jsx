bd8220b0a75d8bc6fc19bac548626dc259d5b4b8
CY-Cheung <c.y.cheung.contact@gmail.com>
2025-08-19 14:54:51 +0800

    Quick commit by script.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,27 +1,31 @@
 import Button from "../../Components/Button/Button";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import DarkVeil from "../../Components/DarkVeil/DarkVeil";
 
 function Home() {
 	const navigate = useNavigate();
 
 	return (
-		<div className="home" onClick={() => document.documentElement.requestFullscreen()}>
-			<div className="menu">
-				<Button
-					text="Screen"
-					fontSize="3dvw"
-					angle={40}
-					onClick={() => navigate("/screen.html")}
-				/>
-				<Button
-					text="Controller"
-					fontSize="3dvw"
-					angle={270}
-					onClick={() => navigate("/controller.html")}
-				/>
+		<>
+			<div className="home">
+				<DarkVeil />
+				<div className="menu">
+					<Button
+						text="Screen"
+						fontSize="3dvw"
+						angle={40}
+						onClick={() => navigate("/screen.html")}
+					/>
+					<Button
+						text="Controller"
+						fontSize="3dvw"
+						angle={270}
+						onClick={() => navigate("/controller.html")}
+					/>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
