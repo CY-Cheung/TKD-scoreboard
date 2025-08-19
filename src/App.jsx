1e726552a2c520d36e2c985440f62defd14fb00a
CY-Cheung <c.y.cheung.contact@gmail.com>
2025-08-19 14:21:25 +0800

    Imported TargetCursor from ReactBitsDev

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Home from "./Pages/Home/Home";
 import Controller from "./Pages/Controller/Controller";
 import Screen from "./Pages/Screen/Screen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TargetCursor from "./Components/TargetCursor/TargetCursor";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/controller.html" element={<Controller />} />
-				<Route path="/screen.html" element={<Screen />} />
-			</Routes>
-		</Router>
+		<>
+			<TargetCursor spinDuration={3.14} hideDefaultCursor={true} />
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/controller.html" element={<Controller />} />
+					<Route path="/screen.html" element={<Screen />} />
+				</Routes>
+			</Router>
+		</>
 	);
 }
 
